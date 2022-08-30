ebf27b7586ca4480151a3da652dac0dfd7d8805c
nazim725 <nazimhabib77@gmail.com>
2022-08-30 10:49:00 -0700

    added

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-const searchProducts=()=>{
-   fetch("https://fakestoreapi.com/products")
-   .then(res=>res.json())
-   .then(data=>showDetails(data))
-
+const searchProducts = () => {
+  fetch('https://fakestoreapi.com/products')
+    .then((res) => res.json())
+    .then((data) => showDetails(data))
 }
 
-const showDetails=(products)=>{
-    const details=document.getElementById("display-card")
+const showDetails = (products) => {
+  const details = document.getElementById('display-card')
 
-    products.forEach(element => {
-        const div=document.createElement('div');
-        div.classList.add('col')
-        div.innerHTML= `
+  products.forEach((element) => {
+    const div = document.createElement('div')
+    div.classList.add('col')
+    div.innerHTML = `
         <div>
-        Hello World
+        <img src="${element.image}" class="card-img-top" alt="..."  height=300 >
         </div>`
-        details.appendChild(div);
-    
-   });
+    details.appendChild(div)
+  })
 }
-
-
-
-
-
-
-
 
 searchProducts()
